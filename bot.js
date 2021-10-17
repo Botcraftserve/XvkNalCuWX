fe57716d5f528baaf030b69b3ae935b81c10a2ef
Sagvi <91565408+Sagvi@users.noreply.github.com>
2021-10-17 20:24:48 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -44,7 +44,7 @@ client.on('message', msg => {
           }
           if (command === "stoneblock") {
             if (args[2] == null) {
-              msg.channel.send('Musisz napisać to w takiej kolejności! ```np !rlcraft 526272 mail haslo``` **HASŁO PRZED WYSŁANIEM ZMIEN NA BYLEJAKIE**') 
+              msg.channel.send('Musisz napisać to w takiej kolejności! ```np !stoneblock 526272 mail haslo``` **HASŁO PRZED WYSŁANIEM ZMIEN NA BYLEJAKIE**') 
               } else {
                 msg.channel.send('Proszę czekać...');
                 let sid = args[0];
@@ -66,10 +66,34 @@ client.on('message', msg => {
                 msg.channel.send('Poczekaj 1-2 minuty i uruchom serwer, pracujemy nad usprawnieniem tej komendy...')
               }
             }
+            if (command === "skyfactory") {
+              if (args[2] == null) {
+                msg.channel.send('Musisz napisać to w takiej kolejności! ```np !skyfactory 526272 mail haslo``` **HASŁO PRZED WYSŁANIEM ZMIEN NA BYLEJAKIE**') 
+                } else {
+                  msg.channel.send('Proszę czekać...');
+                  let sid = args[0];
+                  let id = msg.author.id;
+                  let mail = args[1];
+                  let haslo = args[2];
+                  fs.writeFile('./sid/' + '000' + '.txt', sid, function (err) {
+                    if (err) return console.log(err);
+                  });
+                  fs.writeFile('./sid/' + '001' + '.txt', mail, function (err) {
+                    if (err) return console.log(err);
+                  });
+                  fs.writeFile('./sid/' + '002' + '.txt', haslo, function (err) {
+                    if (err) return console.log(err);
+                  });
+                  PythonShell.run('valhelsia.py', msg.author.id, function (err) {
+                    if (err) throw err;
+                  });
+                  msg.channel.send('Poczekaj 1-2 minuty i uruchom serwer, pracujemy nad usprawnieniem tej komendy...')
+                }
+              }
         });
 
 
-client.login('');
+client.login('ODk3MTY4MTA5Mzk4NTQwMzQ4.YWRu6Q.nk7CK7_bS1mdNYZsXAg1AReFHpk');
 
 function sleep(ms) {
   return new Promise((resolve) => {
--- skyfactory.py
+++ skyfactory.py
@@ -2,7 +2,7 @@ import requests, shutil, datetime
 import time
 
 nazwa = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + '.zip'
-paczka = 'https://media.forgecdn.net/files/3016/966/stoneblock_1.0.37-server.zip'
+paczka = 'https://media.forgecdn.net/files/3012/800/SkyFactory-4_Server_4.2.2.zip'
 sidfile = open("./sid/" + '000' + '.txt',"r+")
 sid = sidfile.read()
 print(sid)
@@ -18,11 +18,11 @@ r = s.post('https://craftserve.pl/login', {'email': mail, 'password': password, 
 r.raise_for_status()
 s.post('https://craftserve.pl/s/' + sid + '/console', 'stop')
 time.sleep(10)
-r = s.delete('https://craftserve.pl/s/936729/settings/restore?mysql=0&files=1')
+r = s.delete('https://craftserve.pl/s/' + sid + '/settings/restore?mysql=0&files=1')
 s.post('https://craftserve.pl/s/' + sid + '/files/?url', {'url_data': paczka})
 time.sleep(15)
-r = s.post('https://craftserve.pl/s/' + sid + '/files_unzip/stoneblock_1.0.37-server.zip', {'destination': '/'})
+r = s.post('https://craftserve.pl/s/' + sid + '/files_unzip/SkyFactory-4_Server_4.2.2.zip', {'destination': '/'})
 time.sleep(10)
-r = s.delete(strona + 'stoneblock_1.0.37-server.zip')
+r = s.delete(strona + 'SkyFactory-4_Server_4.2.2.zip')
 r = s.post('https://craftserve.pl/s/' + sid + '/settings/engines', {'engine': '095e58e8-c63d-4e44-bb7b-d827781c5384', 'step': '1', 'eula': '0', 'java_version': '1'})
 r.raise_for_status()
